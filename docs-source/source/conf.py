0dd0d2438434cb9f4ce2d33c98f155f9986857e5
Jenny Wang <jenny.wang2000@hotmail.com>
2021-06-11 13:53:53 +1000

    Wrong conf.py path?

--- docs-source/source/conf.py
+++ docs-source/source/conf.py
@@ -24,8 +24,6 @@ author = 'Weijie Wang'
 # The full version, including alpha/beta/rc tags
 release = '1.0'
 
-master_doc = 'index'
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
