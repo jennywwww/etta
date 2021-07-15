36ea3bda156cab70fb60afcc8265d6854e903e3a
Jenny Wang <jenny.wang2000@hotmail.com>
2021-07-15 17:36:21 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,25 +3,30 @@ import sys
 import setuptools
 from setuptools import setup
 
-# sys.path.insert(0, "eleanor")
-# I don't really understand the line below
-# from version import __version__
-
+with open('README.md', 'r', encoding='utf-8') as f:
+  long_description = f.read()
 
 setup(
   name='etta',
+  version='0.1.0',
   license='MIT',
   author='Weijie Wang',
   author_email='jenny.wang2000@hotmail.com',
   packages=['etta'],
   url='https://github.com/jennywwww/etta',
+  project_urls={
+    'Bug Tracker': 'https://github.com/jennywwww/etta/issues',
+  },
   description='Wrappers for the ExoFOP-TESS PHP functions.',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
   include_package_data=True,
   package_data={'': ['README.md', 'LICENSE']},
   install_requires=[
     'pandas>=1.0.0',
     'requests>=2.23.0',
-    'setuptools>=41.0.0'],
+    'setuptools>=41.0.0'
+  ],
   classifiers=[
     'Intended Audience :: Science/Research',
     'License :: OSI Approved :: MIT License',
